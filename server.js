ba22d6c9e6193f4288733e4fd1d2892945aa78ef
Victor Onyebuchi <victorchike247@gmail.com>
2021-06-24 14:50:30 +0100

    added software

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/whoami", (req, res) => {
   const ipaddress = req.ip;
   const language = req.headers['accept-language'];
-  
+  const software = req.headers['user-agent'];
 })
 
 
